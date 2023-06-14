f77a26ffb9f4234286015d08e628aeda5781fd58
IvanIsak2000 <vingving648@gmail.com>
2023-06-14 22:38:08 +0300

    added /role

--- src/bot.py
+++ src/bot.py
@@ -3,6 +3,7 @@ from aiogram.types import ContentType, Message
 from config import TOKEN
 import buttons as bt
 
+
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
 
@@ -11,10 +12,29 @@ dp = Dispatcher(bot)
 async def welcome(message: types.Message):
     await message.reply("Hi!\nI'm captcha bot for Telegram", reply_markup=bt.main_menu)
 
+@dp.message_handler(commands=['role'])
+async def echo(message: types.Message):
+    
+
+    async def is_admin(currency_user_id,currency_chat_id):
+        admins = await bot.get_chat_administrators(currency_chat_id)
+
+        for admin in admins:
+            if admin.user.id ==  currency_user_id:
+                return True
+            
+        
+    if await is_admin(message.from_id,message.chat.id):
+        await message.reply('You are an admin 👮‍♂️')
+
+    else:
+        await message.reply('You are a member 🧑‍🌾')
+                
+
+
 
 @dp.message_handler()
 async def echo(message: types.Message):
-
     if message.text == 'Info 🤖':
         await message.reply("I'm a captcha bot for checking user who are he")
 
@@ -23,11 +43,6 @@ async def echo(message: types.Message):
         await message.reply("1.Open bot profile\n2.Copy bot `username`\n3.Go to your chat\n4.Add to chat.\n5.Paste bot `username` in search string.\n6.Click add.")
 
 
-
-dp.message_handler(content_types=[ContentType.NEW_CHAT_MEMBERS])
-async def new_members_handler(message: types.Message):
-    new_member = message.new_chat_members[0]
-    await bot.send_message(message.chat.id, f'Hello! You need to do captcha {new_member.mention}')
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
