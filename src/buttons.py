36bbeba2b503ed70863172ade8d10978110b4f8f
IvanIsak2000 <vingving648@gmail.com>
2023-06-16 09:46:54 +0300

    changed Activate to Status

--- src/buttons.py
+++ src/buttons.py
@@ -5,5 +5,5 @@ from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 # -----Main menu-----
 bt_info =  KeyboardButton('Info 🤖')
 bt_how_to_add = KeyboardButton('How to add im chat? 🤝') 
-bt_activate = KeyboardButton('Activate ✅')
+bt_activate = KeyboardButton('Status')
 main_menu = ReplyKeyboardMarkup(resize_keyboard = True).add(bt_info, bt_how_to_add, bt_activate) 
